d135aad094cd2504148c03579c7e311d0793e9e2
Shivam Kumar <119096144+Shivam29k@users.noreply.github.com>
2022-12-12 22:48:11 +0530

    Add files via upload

---
+++ class3/class3-For_loop&String-CipherSchools.py
@@ -0,0 +1,19 @@
+name = "Harshit"
+for i in range(len(name)):
+    print(name[i])
+print("\n")
+
+name = "harshit"
+for i in name:
+    print(i)
+print("\n")
+
+for i in "mohit":
+    print(i)
+print("\n")
+
+num = input("Enter a number : ")
+total = 0
+for i in num:
+    total += int(i)
+print(total)
---
+++ class5/class5-Delete_data_in_lists-CipherSchools.py
@@ -0,0 +1,17 @@
+# common methods to delete data from list 
+
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi']
+
+# pop method
+# fruits.pop()   # as default pop remove the last index item
+# print(fruits)
+# fruits.pop(1)
+# print(fruits)
+
+# del method
+# del fruits[1]
+# print(fruits)
+
+# remove method
+fruits.remove('pear')
+print(fruits)
